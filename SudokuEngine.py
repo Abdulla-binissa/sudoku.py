f102c04b9520d5c936047434558497b72e507562
Abdulla <abdulla.binissa.176@gmail.com>
2021-01-25 15:19:43 -0800

    Mutable tiles
    Tiles can be written to and changed
    Images replaced with Strings

--- SudokuEngine.py
+++ SudokuEngine.py
@@ -3,7 +3,7 @@ class Gamestate():
     def __init__(self):
 
         self.board = [
-            ['1', '2', '?', '?', '?', '?', '?', '?', '?'],
+            ['1', '2', '3', '?', '?', '?', '?', '?', '?'],
             ['?', '?', '?', '?', '?', '?', '?', '?', '?'],
             ['?', '?', '?', '?', '?', '?', '?', '?', '?'],
             ['?', '?', '?', '?', '?', '?', '?', '?', '?'],
@@ -14,3 +14,20 @@ class Gamestate():
             ['?', '?', '?', '?', '?', '?', '?', '?', '?']
         ]
 
+        self.solution = []
+        self.givenSpots = []
+
+    def isEmptySpot(self, squareSelected):
+        if(self.board[squareSelected[0]][squareSelected[1]] == '?'):
+            return True
+        else: 
+            return False
+
+    def isPlayableSpot(self, squareSelected):
+        if( self.givenSpots.__contains__(squareSelected) ):
+            return False
+        else: 
+            return True
+
+    def setSpot(self, number, squareSelected):
+        self.board[squareSelected[0]][squareSelected[1]] = number
--- SudokuGUI.py
+++ SudokuGUI.py
@@ -5,24 +5,23 @@ WIDTH = HEIGHT = 540
 DIMENSION = 9
 SQ_SIZE = HEIGHT // DIMENSION
 MAX_FPS = 15
-IMAGES = {}
-
-
-def loadImages():
-    digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-    for digit in digits:
-        IMAGES[digit] = pygame.image.load("./images/regularSet/" + digit +".png")
+DIGITS = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 
 def main():
     pygame.init()
 
+    pygame.font.init()
+    font = pygame.font.Font('freesansbold.ttf', 54)
+    
     screen = pygame.display.set_mode(( WIDTH, HEIGHT ))
     clock = pygame.time.Clock()
-    screen.fill(pygame.Color('white'))
     gamestate = SudokuEngine.Gamestate()
 
-    loadImages()
+    pygame.display.set_caption('Sudoku')
+    screen.fill(pygame.Color('white'))
+
+    squareSelected = ()
 
     mainLoop = True
     while mainLoop:
@@ -35,16 +34,22 @@ def main():
                 location = pygame.mouse.get_pos()
                 col = location[0] // SQ_SIZE
                 row = location[1] // SQ_SIZE
+                squareSelected = (row, col)
+                
+            elif ( event.type == pygame.KEYDOWN and gamestate.isPlayableSpot(squareSelected) ):
+                key = event.unicode
+                
+                if DIGITS.__contains__(key):
+                    gamestate.setSpot(key, squareSelected)
 
-                print(row, "-", col)
-
-        drawGameState(screen, gamestate)
+                      
+        drawGameState(screen, gamestate, font)
         clock.tick(MAX_FPS)
         pygame.display.flip()
 
-def drawGameState(screen, gamestate):
-    drawBoard(screen) # Draw Board
-    drawPieces(screen, gamestate) # Draw Pieces
+def drawGameState(screen, gamestate, font):
+    drawBoard(screen) 
+    drawNumbers(screen, gamestate, font)
 
 def drawBoard(screen):
     colors = [pygame.Color("white"), pygame.Color("gray")]
@@ -52,15 +57,14 @@ def drawBoard(screen):
         for c in range(DIMENSION):
             color = colors[((r + c) % 2)]
             pygame.draw.rect(screen, color, pygame.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
-    
-def drawPieces(screen, gamestate):
+
+def drawNumbers(screen, gamestate, font):
     for r in range(DIMENSION):
         for c in range(DIMENSION):
             square = gamestate.board[r][c]
             if( square != '?'):
-                screen.blit(IMAGES[square], pygame.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
-
-
+                textsurface = font.render(square, True, (80, 80, 80))
+                screen.blit(textsurface, pygame.Rect(c*SQ_SIZE + (SQ_SIZE // 4), r*SQ_SIZE + (SQ_SIZE // 8), SQ_SIZE, SQ_SIZE))
 
 
 
